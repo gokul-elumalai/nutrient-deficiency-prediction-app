f6ef6f4b590abff2a3e838f3d4f0e3f12e4c6b6a
gokul-elumalai <src.mv1f3@aleeas.com>
2025-05-26 21:28:22 +0530

    refactor: Add static methods, make columns configurable

---
+++ src/config/config.py
@@ -0,0 +1,8 @@
+NUMERIC_FEATURES = ['height_cm', 'weight_kg', 'cholesterol_level', 'blood_sugar_level', 'daily_steps',
+                    'exercise_frequency', 'sleep_hours', 'calorie_intake', 'protein_intake',
+                    'carbohydrate_intake', 'fat_intake']
+
+
+# Features
+
+BMI = 'bmi'
