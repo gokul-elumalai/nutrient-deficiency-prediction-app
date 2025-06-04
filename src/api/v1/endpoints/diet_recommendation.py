f72cf4650cb3b41b3ec2404bc00a9b7ce4f8adf3
gokul-elumalai <src.mv1f3@aleeas.com>
2025-06-05 00:46:10 +0530

    fix: Send food log count to predict diet

--- src/api/v1/endpoints/diet_recommendation.py
+++ src/api/v1/endpoints/diet_recommendation.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
+from models.food_log import FoodLog
 from models.user_details import UserDetails
 from src.api.v1.debs import CurrentUser, SessionDep
 from prediction_engine import diet_predictor
@@ -21,10 +22,13 @@ def predict_diet(
         select(UserDetails).where(UserDetails.user_id == current_user.id)
     ).first()
 
-    if not user_details:
+    query = select(FoodLog).where(FoodLog.user_id == current_user.id)
+    food_logs = session.exec(query).all()
+
+    if not user_details or not food_logs:
         raise HTTPException(
             status_code=404,
             detail="Please complete your health profile first"
         )
 
-    return {"recommendation": diet_predictor.predict(user_details)}
+    return {"recommendation": diet_predictor.predict(user_details, food_log_count=len(food_logs))}
